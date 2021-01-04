3be40019087a2f51783c5d14866d97e327c63836
William <l0calh34rtz@gmail.com>
2021-01-04 23:53:05 +0700

    add Theme using useContext

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./errorBoundary";
+import ThemeContext from "./ThemeContext";
 class Details extends React.Component {
   // constructor(props) {
   //   super(props);
@@ -37,7 +38,13 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {(themeHook) => (
+              <button style={{ backgroundColor: themeHook[0] }}>
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
